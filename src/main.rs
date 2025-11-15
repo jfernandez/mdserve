98ebeb4856d0b371eb053fe2f8a70225ed43f103
iff <iff@ik.me>
2025-11-15 19:33:21 +0100

    feat: open browser automatically

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,10 @@ struct Args {
     /// Port to serve on
     #[arg(short, long, default_value = "3000")]
     port: u16,
+
+    /// Open in browser on start
+    #[arg(short, long, default_value_t = false)]
+    open: bool,
 }
 
 #[tokio::main]
@@ -52,6 +56,7 @@ async fn main() -> Result<()> {
         is_directory_mode,
         args.hostname,
         args.port,
+        args.open,
     )
     .await?;
 
